0463be2e913f9e575b55dd144c90cac0e5a89e88
Mohammad Rajaul Islam <springapidev@gmail.com>
2017-11-03 21:49:42 +0600

    xml startup example

---
+++ Ex01-well-formed-xml/src/com/coderbd/readusingsax/ReadXMLFileUsingSax.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.coderbd.readusingsax;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class ReadXMLFileUsingSax {
+    
+   public static void main(String argv[]) {
+
+    try {
+
+	SAXParserFactory factory = SAXParserFactory.newInstance();
+	SAXParser saxParser = factory.newSAXParser();
+
+	DefaultHandler handler = new DefaultHandler() {
+
+	boolean bfname = false;
+	boolean bsalary = false;
+
+        @Override
+	public void startElement(String uri, String localName,String qName,
+                Attributes attributes) throws SAXException {
+
+		System.out.println("Start Element :" + qName);
+
+		if (qName.equalsIgnoreCase("FULLNAME")) {
+			bfname = true;
+		}
+		
+		if (qName.equalsIgnoreCase("SALARY")) {
+			bsalary = true;
+		}
+
+	}
+
+        @Override
+	public void endElement(String uri, String localName,
+		String qName) throws SAXException {
+
+		System.out.println("End Element :" + qName);
+
+	}
+
+	public void characters(char ch[], int start, int length) throws SAXException {
+
+		if (bfname) {
+			System.out.println("Full Name : " + new String(ch, start, length));
+			bfname = false;
+		}
+
+		if (bsalary) {
+			System.out.println("Salary : " + new String(ch, start, length));
+			bsalary = false;
+		}
+
+	}
+
+     };
+
+       saxParser.parse("E:\\git\\xml\\Ex01-well-formed-xml\\src\\com\\coderbd\\readusingsax/staff.xml", handler);
+
+     } catch (Exception e) {
+       e.printStackTrace();
+     }
+
+   }
+
+}
+
